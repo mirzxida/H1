3497f1174d2baf37490ddb74f6137d3176f91a08
Hilola <mirzabekovahilola14@gmail.com>
2026-01-24 00:16:52 +0500

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
-public class Main{
+import model.*;
+import service.FestivalService;
 
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        FestivalService service = new FestivalService();
+
+        Band band = new Band(1, "Echo Pulse", "USA", "Rock");
+        Stage stage = new Stage(1, "Main Stage", 5000);
+
+        Slot slot = new Slot(
+                band,
+                stage,
+                LocalDateTime.of(2026,7,1,18,0),
+                LocalDateTime.of(2026,7,1,19,0)
+        );
+
+        try {
+            service.scheduleSlot(slot);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
